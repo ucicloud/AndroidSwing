19b9739d43367d36cff9cec094381cc5e63956c4
yyWater <only_app@163.com>
2017-11-01 20:38:26 +0800

    手表电量缓存方法

--- Swing/app/src/main/java/com/kidsdynamic/swing/domain/DeviceManager.java
+++ Swing/app/src/main/java/com/kidsdynamic/swing/domain/DeviceManager.java
@@ -1,6 +1,10 @@
 package com.kidsdynamic.swing.domain;
 
 import android.content.Context;
+import android.text.TextUtils;
+
+import com.kidsdynamic.data.persistent.PreferencesUtil;
+import com.kidsdynamic.swing.SwingApplication;
 
 /**
  * Created by Administrator on 2017/10/27.
@@ -10,5 +14,46 @@ public class DeviceManager {
     public static String getFocusWatchName(Context context){
         // TODO: 2017/10/27 查询当前使用的手表名称
         return "My";
+    }
+
+    public static String getMacID(String macAddress) {
+        String[] separated = macAddress.split(":");
+        String macId = "";
+        for (String s : separated)
+            macId += s;
+
+        //macId = "1A2B3C4D5F01";
+        return macId;
+    }
+
+    public static String getMacAddress(String macId) {
+        if (macId.length() < 12)
+            return "00:00:00:00:00:00";
+
+        return String.format("%c%c:%c%c:%c%c:%c%c:%c%c:%c%c",
+                macId.charAt(0), macId.charAt(1), macId.charAt(2), macId.charAt(3),
+                macId.charAt(4), macId.charAt(5), macId.charAt(6), macId.charAt(7),
+                macId.charAt(8), macId.charAt(9), macId.charAt(10), macId.charAt(11)
+        );
+    }
+
+    public static boolean saveBindWatchBattery(String watchMacAddress, int battery){
+        if(TextUtils.isEmpty(watchMacAddress) || battery <= 0){
+            return false;
+        }
+
+        String macID = getMacID(watchMacAddress);
+
+        PreferencesUtil preferencesUtil = PreferencesUtil.getInstance(SwingApplication.getAppContext());
+        return preferencesUtil.setPreferenceIntValue(macID, battery);
+    }
+
+    public static int getCacheWatchBattery(String watchMacId){
+        if(TextUtils.isEmpty(watchMacId)){
+            return -1;
+        }
+        PreferencesUtil preferencesUtil = PreferencesUtil.getInstance(SwingApplication.getAppContext());
+        return preferencesUtil.gPrefIntValue(watchMacId);
+
     }
 }
